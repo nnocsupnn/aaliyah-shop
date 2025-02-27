b03e208fbc47fd89d697544d11369bdb3cfe764c
nnocsupnn <nincas21@gmail.com>
2025-02-27 17:42:10 +0800

    feat: add analytics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import ShopList from '@/components/ShopList.vue';
+import { Analytics } from "@vercel/analytics/vue"
+
 </script>
 
 <template>
   <div id="app">
+    <Analytics/>
     <ShopList />
   </div>
   <RouterView/>
